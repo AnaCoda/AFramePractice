82b09049fb4c1902954211ea95395c23092209c1
AnaCoda <anacod@zoho.com>
2021-12-02 10:50:45 -0700

    restrict angle of rotation

--- pendulumAngle.js
+++ pendulumAngle.js
@@ -1,15 +1,10 @@
 AFRAME.registerComponent('pendulum-angle', {
     init: function() {
-        // Solution for Getting Entities.
-        //this.pendulum = document.querySelector('#pend');
-
         this.text = document.querySelector('#angle-text');
         console.log(this.text);
     },
     tick: function() {
         var angle = Math.round(((90 - Math.abs(this.el.getAttribute('rotation').y)) * 10)) / 10;
-        console.log(angle);
         this.text.setAttribute('text', 'value', angle.toString());
-        console.log(this.text);
     }
 });
