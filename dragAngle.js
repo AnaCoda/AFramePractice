1570ccbb0dfeb0c7693d744e733642e2915459b5
AnaCoda <anacod@zoho.com>
2021-12-06 09:10:55 -0700

    hand controls

--- dragAngle.js
+++ dragAngle.js
@@ -1,6 +1,9 @@
 AFRAME.registerComponent('drag-angle', {
     init: function() {
         var el = this.el; // <a-box>
+        this.ifMouseDown = false;
+        this.x_cord = 0;
+        this.y_cord = 0;
         el.addEventListener('mouseenter', function() {
             el.setAttribute('color', '#24CAFF');
             console.log('mouseenter');
@@ -8,9 +11,8 @@ AFRAME.registerComponent('drag-angle', {
         el.addEventListener('mouseleave', function() {
             el.setAttribute('color', '#EF2D5E');
         });
-        el.addEventListener('click', function() {
-            el.setAttribute('scale', { x: 2, y: 1, z: 2 });
-            console.log('click');
-        });
+        el.addEventListener('mousemove', function() {
+            el.setAttribute('color', '#FFFFFF');
+        })
     }
 });
