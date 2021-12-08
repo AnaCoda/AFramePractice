643f916f7e742f1a0b4a74f6b1b66d12a9d455fe
AnaCoda <anacod@zoho.com>
2021-12-08 08:57:41 -0700

    changed materials

--- super-hands.js
+++ super-hands.js
@@ -1,7 +1,18 @@
 (function() {
     function r(e, n, t) {
-        function o(i, f) { if (!n[i]) { if (!e[i]) { var c = "function" == typeof require && require; if (!f && c) return c(i, !0); if (u) return u(i, !0); var a = new Error("Cannot find module '" + i + "'"); throw a.code = "MODULE_NOT_FOUND", a } var p = n[i] = { exports: {} };
-                e[i][0].call(p.exports, function(r) { var n = e[i][1][r]; return o(n || r) }, p, p.exports, r, e, n, t) } return n[i].exports } for (var u = "function" == typeof require && require, i = 0; i < t.length; i++) o(t[i]); return o } return r })()({
+        function o(i, f) {
+            if (!n[i]) {
+                if (!e[i]) { var c = "function" == typeof require && require; if (!f && c) return c(i, !0); if (u) return u(i, !0); var a = new Error("Cannot find module '" + i + "'"); throw a.code = "MODULE_NOT_FOUND", a }
+                var p = n[i] = { exports: {} };
+                e[i][0].call(p.exports, function(r) { var n = e[i][1][r]; return o(n || r) }, p, p.exports, r, e, n, t)
+            }
+            return n[i].exports
+        }
+        for (var u = "function" == typeof require && require, i = 0; i < t.length; i++) o(t[i]);
+        return o
+    }
+    return r
+})()({
     1: [function(require, module, exports) {
         'use strict';
 
@@ -827,7 +838,9 @@
             schema: {
                 maxGrabbers: { type: 'int', default: NaN },
                 invert: { default: false },
-                suppressY: { default: false }
+                suppressY: { default: false },
+                suppressX: { default: false },
+                suppressZ: { default: false }
             },
             init: function() {
                 this.GRABBED_STATE = 'grabbed';
@@ -856,8 +869,8 @@
             },
             update: function() {
                 this.physicsUpdate();
-                this.xFactor = this.data.invert ? -1 : 1;
-                this.zFactor = this.data.invert ? -1 : 1;
+                this.xFactor = (this.data.invert ? -1 : 1) * !this.data.suppressX;
+                this.zFactor = (this.data.invert ? -1 : 1) * !this.data.suppressZ;
                 this.yFactor = (this.data.invert ? -1 : 1) * !this.data.suppressY;
             },
             tick: function() {
