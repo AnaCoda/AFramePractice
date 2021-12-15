50b8da73a144f52942009087614025dd9e3a2e44
AnaCoda <anacod@zoho.com>
2021-12-15 10:03:10 -0700

    wrong import

--- physicsFriction.js
+++ physicsFriction.js
@@ -3,7 +3,7 @@ AFRAME.registerComponent("physics-frictions", {
         var sceneEl = this.el;
         var floorEl = sceneEl.querySelector("#ground");
         var boxEl = sceneEl.querySelector(".block");
-        floorEl.body.setFriction(0.001);
-        boxEl.body.setFriction(0.001);
+        floorEl.body.setFriction(5);
+        boxEl.body.setFriction(5);
     },
 });
